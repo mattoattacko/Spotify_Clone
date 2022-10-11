419d8cc74fe35e3d95eb998d9399338f7553905b
unknown <matt.mcquain@gmail.com>
2022-10-10 18:44:09 -0700

    working on the TopPlay component

--- src/components/TopPlay.jsx
+++ src/components/TopPlay.jsx
@@ -47,11 +47,11 @@ const TopPlay = () => {
       <div className='w-full flex flex-col'>
         <div className='flex flex-row justify-between items-center'>
           <h2 className='text-white font-bold text-2xl'>
-            Top of Charts
+            Top Charts
           </h2>
           <Link to='/top-charts'>
             <p className='text-gray-300 text-base cursor-pointer'>
-              See more...
+              See More
             </p>
           </Link>
         </div>
@@ -66,6 +66,49 @@ const TopPlay = () => {
           ))}
         </div>
       </div>
+
+      {/* Top Artist */}
+
+      <div className='w-full flex flex-col mt-8'>
+        <div className='flex flex-row justify-between items-center'>
+          <h2 className='text-white font-bold text-2xl'>
+            Top Artists
+          </h2>
+          <Link to='/top-artists'>
+            <p className='text-gray-300 text-base cursor-pointer'>
+              See More
+            </p>
+          </Link>
+        </div>
+
+        <Swiper
+          slidesPerView='auto'
+          spaceBetween={15}
+          freeMode
+          centeredSlides
+          centeredSlidesBounds
+          modules={[FreeMode]}
+          className='mt-4'
+        >
+          {/* loop over top artists */}
+          {topPlays?.map((song, i) => (
+            <SwiperSlide 
+              key={song?.key}
+              style={{ width: '25%', height: 'auto' }}
+              className='shadow-lg rounded-full animate-slideright'
+            >
+              <Link to={`/artists/${song?.artists[0].adamid}`}>
+                <img 
+                  src={song?.images.background} 
+                  className='rounded-full w-full object-cover' 
+                  alt='name'
+                />
+              </Link>
+            </SwiperSlide>
+          ))}
+        </Swiper>
+      </div>
+      
     </div>
   );
 };
