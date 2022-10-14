35477bdb418fa7310b80b8f991834bf47beb4111
unknown <matt.mcquain@gmail.com>
2022-10-13 20:17:13 -0700

    added API call to geo.ipify and its routing in ShazamCore

--- src/pages/AroundYou.jsx
+++ src/pages/AroundYou.jsx
@@ -1,5 +1,35 @@
-import React from 'react';
+/* eslint-disable */
 
-const CountryTracks = () => <div>CountryTracks</div>;
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+import { useSelector } from 'react-redux';
 
-export default CountryTracks;
+import { Error, Loader, SongCard } from '../components';
+
+const AroundYou = () => {
+
+  const [country, setCountry] = useState('');
+  const [loading, setLoading] = useState(true);
+  const { activeSong, isPlaying } = useSelector((state) => state.player); //pulls data from state
+
+  console.log(country)
+
+  // using geo.ipify api to get country
+  useEffect(() => {
+    axios.get(`https://geo.ipify.org/api/v2/country?apiKey=at_ymw43KFIQBNqnyECR4EZ47WKipNxq`)
+      .then((res) => setCountry(res?.data?.location?.country))
+      .catch((err) => console.error(err))
+      .finally(() => setLoading(false));
+  }, [country]);
+
+  //to fetch the songs around us we need to know where we are located. We recall it whenever the country changes.
+  return (
+    <div>
+
+    </div>
+  )  
+}
+
+
+
+export default AroundYou;
--- src/redux/services/shazamCore.js
+++ src/redux/services/shazamCore.js
@@ -19,6 +19,7 @@ export const shazamCoreApi = createApi({
     getSongDetails: builder.query({ query: ({ songid }) => `/tracks/details?track_id=${ songid }` }),
     getSongRelated: builder.query({ query: ({ songid }) => `/tracks/related?track_id=${ songid }` }),
     getArtistDetails: builder.query({ query: (artistId) => `/artists/details?artist_id=${ artistId }` }),
+    getSongsByCountry: builder.query({ query: (countryCode) => `charts/country?country_code=${countryCode}` }),
   }),
 });
 
@@ -28,4 +29,5 @@ export const {
   useGetSongDetailsQuery,
   useGetSongRelatedQuery,
   useGetArtistDetailsQuery,
+  useGetSongsByCountryQuery,
 } = shazamCoreApi;
